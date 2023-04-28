84a8a387aff0219fb46542f157c076d76966bf82
Aline Rosa <alinerosa_s@hotmail.com>
2023-04-28 16:47:18 -0300

    Finalizando seção 3
    Seção 3 finalizada

--- Seção - 02/app.js
+++ Seção - 02/app.js
@@ -7,7 +7,17 @@ let sobrenome = 'Silva';
 let idade = '24';
 const numCliente = 095864;
 let valorEmprestimo = 150000;
-let taxaDeJuros = 10;
+let taxaDeJuros = 0.10;
+let numAnos = 6
 let ehBomPagador = true;
 
 console.log('Boa tarde Sr. ' + nome + ' ' + sobrenome + '. Sua idade é: ' + idade);
+
+/* Montante = valorEmprestimo + juros
+Formula: juros = valorEmprestimo * taxaDeJuros * numAnos 
+*/
+
+let juros = valorEmprestimo * taxaDeJuros * numAnos;
+let montante = valorEmprestimo + juros;
+
+console.log('Valor do montante: ' + montante)
---
+++ Seção - 03/Desafio - 02/media.js
@@ -0,0 +1,8 @@
+let notaPrimeiroBimestre = 7;
+let notaSegundoBimestre = 8;
+let notaTerceiroBimestre = 9;
+let notaQuartoBimestre = 8;
+let media;
+
+media = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre)/4;
+console.log('A média é: ' + media);
