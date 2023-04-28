474e7f908f1dcf195ae8906b1765931ed123cc99
Aline Rosa <alinerosa_s@hotmail.com>
2023-04-28 15:32:45 -0300

    Primeiro commit
    Adicionando arquivos

---
+++ Seção - 02/app.js
@@ -0,0 +1,13 @@
+/* 
+Sistema de cadastro de clientes
+*/
+
+let nome = 'Manuel';
+let sobrenome = 'Silva';
+let idade = '24';
+const numCliente = 095864;
+let valorEmprestimo = 150000;
+let taxaDeJuros = 10;
+let ehBomPagador = true;
+
+console.log('Boa tarde Sr. ' + nome + ' ' + sobrenome + '. Sua idade é: ' + idade);
---
+++ Seção - 02/desafio - 01/app.js
@@ -0,0 +1,8 @@
+let nomeProduto = 'Toddy';
+let precoProduto = 6.00;
+let possuiValidade = true;
+
+console.log('Olá! Segue as informações do produto abaixo:');
+console.log(nomeProduto);
+console.log('R$' + precoProduto + ',00');
+console.log(possuiValidade);
